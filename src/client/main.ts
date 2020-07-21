4ef8ee911e3205752a6f1fe68aea2c7057b2277d
dnacu <winteri1276@gmail.com>
2020-07-21 21:29:45 +0900

    feat: column drag and drop 구현
    - 여러 컬럼을 한번에 건너 뛸 경우에 대한 핸들링 필요

--- src/client/main.ts
+++ src/client/main.ts
@@ -2,6 +2,7 @@ import './styles/globalstyle.scss'
 import './styles/page.scss'
 
 import './scripts/card-dnd'
+import './scripts/column-dnd'
 
 import './scripts/init.ts'
 
---
+++ src/client/scripts/column-dnd.ts
@@ -0,0 +1,229 @@
+import { eventCollector } from '@/client/utils/event-collector'
+
+const COLUMN_MARGIN_LEFT = 25
+const COLUMN_WIDTH = 353
+const COLUMN_FULL_WIDTH = COLUMN_MARGIN_LEFT + COLUMN_WIDTH
+
+const getTransformX = (el: HTMLElement): number => {
+  return parseInt(el?.style?.transform?.split('px')[0]?.split('(')[1]) || 0
+}
+
+// pointerup이 되면 실행해야지
+// document.body.classList.remove('hovered')
+
+window.addEventListener('pointerdown', (e) => {
+  const target = e.target as HTMLElement
+
+  const originalColumn = target.closest('.column:not(.new)') as HTMLElement
+  const originalHeader = target.closest('.column-header') as HTMLElement
+
+  if (!originalHeader) return
+
+  const initialX = e.pageX
+  const initialY = e.pageY
+
+  const longPressTimeOut = setTimeout(() => {
+    document.body.classList.add('hovered')
+
+    Array.from(originalColumn.parentNode.children).map((el, idx) => {
+      el.setAttribute('from-index', `${idx}`)
+      el.setAttribute('to-index', `${idx}`)
+    })
+
+    // Create a ghost column which is moving along the pointer
+    const ghostColumn = originalColumn.cloneNode(true) as HTMLElement
+    const placeholder = originalColumn.cloneNode() as HTMLElement
+
+    // Initialize the ghost card and placeholder CSS properties and class name
+    ghostColumn.style.pointerEvents = placeholder.style.pointerEvents = 'none'
+    ghostColumn.style.position = 'fixed'
+    ghostColumn.style.margin = '0px'
+    ghostColumn.style.width = placeholder.style.width = `${originalColumn.clientWidth}px`
+    ghostColumn.style.height = placeholder.style.height = `${originalColumn.clientHeight}px`
+
+    const columnRect = originalColumn.getBoundingClientRect()
+
+    const index = Array.from(originalColumn.parentNode.children).indexOf(
+      originalColumn
+    )
+
+    // Style the placeholder
+    placeholder.classList.add('placeholder')
+    placeholder.style.position = 'absolute'
+    placeholder.style.left = '0px'
+    placeholder.style.margin = '0px'
+    placeholder.style.transform = `translate3d(${columnRect.left}px, 0, 0)`
+
+    // Prepend placeholder
+    originalColumn.parentElement.prepend(placeholder)
+
+    const placeholderRect = placeholder.getBoundingClientRect()
+
+    const originalColumnLeft = placeholderRect.left
+    const originalColumnTop = placeholderRect.top
+
+    // Style the ghost card
+    ghostColumn.classList.add('ghost')
+    ghostColumn.style.transition = 'box-shadow 200ms ease, transform 200ms ease'
+    ghostColumn.style.top = `${placeholderRect.top}px`
+    ghostColumn.style.left = `${placeholderRect.left}px`
+    ghostColumn.style.zIndex = '9999'
+    ghostColumn.style.opacity = '0.9'
+
+    // Insert into the DOM
+    originalColumn.parentElement.appendChild(ghostColumn)
+
+    // Trick for activating transitions
+    ghostColumn.getBoundingClientRect()
+
+    ghostColumn.style.boxShadow = '0 22px 60px rgba(0, 0, 0, 0.3)'
+    ghostColumn.style.transform = ghostColumn.style.webkitTransform =
+      'scale(1.05)'
+
+    // Hide the original card
+    originalColumn.style.visibility = 'hidden'
+
+    eventCollector.add(window, 'pointermove', (e: MouseEvent) => {
+      const deltaX = e.pageX - initialX
+      const deltaY = e.pageY - initialY
+
+      ghostColumn.style.top = `${originalColumnTop + deltaY}px`
+      ghostColumn.style.left = `${originalColumnLeft + deltaX}px`
+
+      const ghostColumnRect = ghostColumn.getBoundingClientRect()
+
+      // Detect a card based on the coordinate of
+      // the center of the ghost card
+      const hoveredColumn = document
+        .elementFromPoint(
+          ghostColumnRect.left + ghostColumnRect.width / 2,
+          ghostColumnRect.top + ghostColumnRect.height / 2
+        )
+        ?.closest('.column:not(.new)') as HTMLElement
+      //   ||
+      // (document
+      //   .elementFromPoint(e.pageX, e.pageY)
+      //   ?.closest('.column:not(.new)') as HTMLElement)
+
+      // if (!hoveredColumn || document.body.classList.contains('hovered'))
+      if (!hoveredColumn || hoveredColumn.classList.contains('animating'))
+        return
+
+      const hoveredColumnRect = hoveredColumn.getBoundingClientRect()
+      const currentPlaceholderLeft = getTransformX(placeholder)
+      const currentPlaceholderRight = currentPlaceholderLeft + COLUMN_WIDTH
+
+      // 왼쪽에서 오른쪽으로 움직일 때
+      if (currentPlaceholderLeft <= hoveredColumnRect.left) {
+        hoveredColumn.classList.add('animating')
+        const prevIdx = parseInt(hoveredColumn.getAttribute('to-index'))
+        hoveredColumn.setAttribute('to-index', `${prevIdx - 1}`)
+        eventCollector.add(hoveredColumn, 'transitionend', (e) => {
+          hoveredColumn.classList.remove('animating')
+          eventCollector.remove(hoveredColumn, 'transitionend')
+        })
+
+        placeholder.style.transform = `translate3d(${
+          currentPlaceholderLeft + 378
+        }px, 0, 0)`
+
+        const hoveredColumnLeft = getTransformX(hoveredColumn)
+        hoveredColumn.style.transform = `translate3d(${
+          hoveredColumnLeft - COLUMN_FULL_WIDTH
+        }px, 0, 0)`
+
+        const idx = parseInt(placeholder.getAttribute('to-index'))
+        placeholder.setAttribute('to-index', `${idx + 1}`)
+      }
+      if (hoveredColumnRect.right <= currentPlaceholderRight) {
+        {
+          hoveredColumn.classList.add('animating')
+          const prevIdx = parseInt(hoveredColumn.getAttribute('to-index'))
+          hoveredColumn.setAttribute('to-index', `${prevIdx + 1}`)
+          eventCollector.add(hoveredColumn, 'transitionend', (e) => {
+            hoveredColumn.classList.remove('animating')
+            eventCollector.remove(hoveredColumn, 'transitionend')
+          })
+
+          placeholder.style.transform = `translate3d(${
+            currentPlaceholderLeft - 378
+          }px, 0, 0)`
+
+          const hoveredColumnLeft = getTransformX(hoveredColumn)
+          hoveredColumn.style.transform = `translate3d(${
+            hoveredColumnLeft + COLUMN_FULL_WIDTH
+          }px, 0, 0)`
+
+          const idx = parseInt(placeholder.getAttribute('to-index'))
+          placeholder.setAttribute('to-index', `${idx - 1}`)
+        }
+      }
+    })
+
+    eventCollector.add(window, 'pointerup', (e: MouseEvent) => {
+      document.body.classList.remove('hovered')
+      eventCollector.remove(window, 'pointermove')
+
+      const fromIdx = placeholder.getAttribute('from-index')
+      const toIdx = placeholder.getAttribute('to-index')
+
+      ghostColumn.style.transition = `top 200ms ease, left 200ms ease, box-shadow 200ms ease, transform 200ms ease`
+
+      ghostColumn.style.boxShadow = 'none'
+      ghostColumn.style.transform = ghostColumn.style.webkitTransform = ''
+
+      const placeholderRect = placeholder.getBoundingClientRect()
+
+      ghostColumn.style.top = `${placeholderRect.top}px`
+      ghostColumn.style.left = `${placeholderRect.left}px`
+
+      ghostColumn.addEventListener('transitionend', function tec() {
+        ghostColumn.remove()
+        placeholder.remove()
+
+        ghostColumn.removeEventListener('transitionend', tec)
+
+        // ghostColumn.remove()
+        originalColumn.setAttribute('to-index', toIdx)
+
+        const result = Array.from(originalColumn.parentNode.children)
+          .sort((a, b) => {
+            return (
+              parseInt(a.getAttribute('to-index')) -
+              parseInt(b.getAttribute('to-index'))
+            )
+          })
+          .map((elm) => {
+            const clonedElm = elm.cloneNode(true) as HTMLElement
+            clonedElm.style.transform = null
+            clonedElm.removeAttribute('from-index')
+            clonedElm.removeAttribute('to-index')
+            clonedElm.style.visibility = null
+            return clonedElm
+          })
+
+        const parentElm = originalColumn.parentElement
+
+        parentElm.innerHTML = ``
+        result.forEach((elm) => parentElm.appendChild(elm))
+      })
+    })
+  }, 200)
+
+  eventCollector.add(window, 'pointerup', (e: MouseEvent) => {
+    clearTimeout(longPressTimeOut)
+    eventCollector.remove(window, 'pointerup')
+  })
+
+  eventCollector.add(window, 'pointermove', (e: MouseEvent) => {
+    const absDeltaX = Math.abs(e.pageX - initialX)
+    const absDeltaY = Math.abs(e.pageY - initialY)
+
+    const absDistance = Math.sqrt(absDeltaX ** 2 + absDeltaY ** 2)
+
+    if (absDistance > 3) {
+      clearTimeout(longPressTimeOut)
+      eventCollector.remove(window, 'pointermove')
+    }
+  })
+})
