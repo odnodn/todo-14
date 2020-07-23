f031182d5de74fd39ebd568a286ef662760933f7
jhaemin <io@jhaemin.com>
2020-07-23 11:11:21 +0900

    fix: run the script after dom is completely parsed

--- src/client/scripts/activity.ts
+++ src/client/scripts/activity.ts
@@ -1,23 +1,28 @@
-const activityBtn = document.querySelector<HTMLElement>('.activity-btn')
-const activityJumbo = document.querySelector<HTMLElement>('.activity-jumbo')
-const activityClose = document.querySelector<HTMLElement>('.activity-close')
+window.addEventListener('DOMContentLoaded', () => {
+  const activityBtn = document.querySelector<HTMLElement>('.activity-btn')
+  const activityJumbo = document.querySelector<HTMLElement>('.activity-jumbo')
+  const activityClose = document.querySelector<HTMLElement>('.activity-close')
 
-activityClose.addEventListener('click', function cc() {
-  activityJumbo.classList.add('hidden')
-})
-
-activityBtn.addEventListener('click', () => {
-  activityJumbo.classList.remove('hidden')
-})
-
-window.addEventListener('click', function cc(e) {
-  const target = e.target
-
-  if (!(target instanceof HTMLElement)) {
-    return
-  }
-
-  if (!target.closest('.activity-jumbo') && !target.closest('.activity-btn')) {
+  activityClose.addEventListener('click', function cc() {
     activityJumbo.classList.add('hidden')
-  }
+  })
+
+  activityBtn.addEventListener('click', () => {
+    activityJumbo.classList.remove('hidden')
+  })
+
+  window.addEventListener('click', function cc(e) {
+    const target = e.target
+
+    if (!(target instanceof HTMLElement)) {
+      return
+    }
+
+    if (
+      !target.closest('.activity-jumbo') &&
+      !target.closest('.activity-btn')
+    ) {
+      activityJumbo.classList.add('hidden')
+    }
+  })
 })
