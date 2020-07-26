b4d9d9a40455f4c6b4031b9ade1198b6e6ca0d35
jhaemin <io@jhaemin.com>
2020-07-26 23:12:19 +0900

    fix: decremental time comparison
    * Compare by the sequence of year -> month -> day -> minute
    * Now correctly compare two based on the absolute time, not only with the year diff

--- src/client/modules/friendly-time.ts
+++ src/client/modules/friendly-time.ts
@@ -8,37 +8,21 @@ export const friendlyTime = (time: Date | string): string => {
   const now = dayjs()
   const atObj = dayjs(time).add(9, 'hour')
 
-  const secDiff = now.diff(atObj, 'second')
-  if (secDiff < 10) {
-    timeString = '방금'
-    return timeString
-  } else if (secDiff < 60) {
-    timeString = `${secDiff}초 전`
-    return timeString
+  const minDiff = now.diff(atObj, 'minute')
+  const dayDiff = now.diff(atObj, 'day')
+  const monthDiff = now.diff(atObj, 'month')
+  const yearDiff = now.diff(atObj, 'year')
+
+  if (yearDiff >= 1) {
+    timeString = `${yearDiff} year${yearDiff > 1 ? 's' : ''} ago`
+  } else if (monthDiff >= 1) {
+    timeString = `${monthDiff} month${monthDiff > 1 ? 's' : ''} ago`
+  } else if (dayDiff >= 1) {
+    timeString = `${dayDiff} day${dayDiff > 1 ? 's' : ''} ago`
+  } else if (minDiff >= 1) {
+    timeString = `${minDiff} min${minDiff > 1 ? 's' : ''} ago`
   } else {
-    const minDiff = now.diff(atObj, 'minute')
-    if (minDiff > 0 && minDiff < 60) {
-      timeString = `${minDiff}분 전`
-      return timeString
-    } else {
-      const hourDiff = now.diff(atObj, 'hour')
-      const dayDiff = now.diff(atObj, 'day')
-      const monthDiff = now.diff(atObj, 'month')
-      const yearDiff = now.diff(atObj, 'year')
-      if (hourDiff > 0 && hourDiff < 24) {
-        timeString = `${hourDiff}시간 전`
-        return timeString
-      } else if (now.day() !== atObj.day() && now.month() === atObj.month()) {
-        timeString = `${dayDiff}일 전`
-        return timeString
-      } else if (now.month() !== atObj.month() && now.year() === atObj.year()) {
-        timeString = `${monthDiff}달 전`
-        return timeString
-      } else if (now.year() !== atObj.year()) {
-        timeString = `${yearDiff || 1}년 전`
-        return timeString
-      }
-    }
+    timeString = `now`
   }
 
   return timeString
