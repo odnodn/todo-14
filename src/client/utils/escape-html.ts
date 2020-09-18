764342776a2e5fd912254a7ef192905d96286f09
dnacu <winteri1276@gmail.com>
2020-09-18 14:58:57 +0900

    feat: define escapeHtml function(#48) - to prevent insert html tag to our input

---
+++ src/client/utils/escape-html.ts
@@ -0,0 +1,13 @@
+export const escapeHtml = (str: string) => {
+  const map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;',
+  }
+
+  return str.replace(/[&<>"']/g, (m) => {
+    return map[m]
+  })
+}
