9070f67419b191374f261bdd949706a1367fbe7d
dnacu <winteri1276@gmail.com>
2020-09-22 16:37:52 +0900

    feat: connect client to server through socket

--- src/client/main.ts
+++ src/client/main.ts
@@ -12,6 +12,8 @@ import './scripts/activity.ts'
 import './scripts/color-scheme.ts'
 import { resetDatabaseAPI } from './api/reset-database'
 
+import io from 'socket.io-client'
+
 function reset() {
   resetDatabaseAPI(
     parseInt(document.querySelector('.app').getAttribute('board-id'))
@@ -25,6 +27,35 @@ window.addEventListener('load', () => {
     if (e.key === 'z' && e.metaKey && e.shiftKey) {
       reset()
     }
+  })
+
+  const socket = io('ws://localhost:12100')
+
+  socket.on('hello2', (data) => {
+    console.log(data)
+  })
+
+  socket.on('connect', () => {
+    // either with send()
+    // socket.send('Hello!')
+
+    // or with emit() and custom event names
+    socket.emit(
+      'salutations',
+      'Hello222!',
+      { mr: 'john' },
+      Uint8Array.from([1, 2, 3, 4])
+    )
+  })
+
+  // handle the event sent with socket.send()
+  socket.on('message', (data) => {
+    console.log(data)
+  })
+
+  // handle the event sent with socket.emit()
+  socket.on('hello_world', (data) => {
+    console.log(data)
   })
 
   document
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,3 +1,24 @@
 import { PORT, app } from './app'
+import SocketIO from 'socket.io'
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+const io = SocketIO(server)
+
+io.on('connect', (socket) => {
+  console.log('hello world!')
+
+  socket.emit('hello_world', {
+    say: 'hello!',
+  })
+
+  socket.send({
+    say: 'hello!',
+  })
+
+  socket.on('salutations', (...data) => {
+    console.log(data)
+  })
+})
+
+export { io as socket }
