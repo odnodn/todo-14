1bc598cf6f9d5b3d6536cb37be4ebb0dca0393f5
jhaemin <io@jhaemin.com>
2020-09-22 17:48:19 +0900

    chore: remove test codes

--- src/client/main.ts
+++ src/client/main.ts
@@ -44,26 +44,3 @@ socket.on('card', ([data]) => {
     createCardClient(payload.columnId, payload.cardId, payload.content)
   }
 })
-
-socket.on('connect', () => {
-  // either with send()
-  // socket.send('Hello!')
-
-  // or with emit() and custom event names
-  socket.emit(
-    'salutations',
-    'Hello222!',
-    { mr: 'john' },
-    Uint8Array.from([1, 2, 3, 4])
-  )
-})
-
-// handle the event sent with socket.send()
-socket.on('message', (data) => {
-  console.log(data)
-})
-
-// handle the event sent with socket.emit()
-socket.on('hello_world', (data) => {
-  console.log(data)
-})
--- src/server/main.ts
+++ src/server/main.ts
@@ -6,10 +6,6 @@ const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 const io = SocketIO(server)
 
 io.on('connect', (socket) => {
-  socket.on('salutations', (...data) => {
-    console.log(data)
-  })
-
   socket.on('card', (...data) => {
     socket.broadcast.emit('card', {
       ...data,
